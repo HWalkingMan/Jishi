2ddba4d60b2cb4453f2407053ed2283a831c4bd7
HWalkingMan <971813982@qq.com>
2020-02-28 22:17:19 +0800

    message V1.0.3

--- app/src/main/java/com/jishi/jishi/ui/fragment/MessageFragment.java
+++ app/src/main/java/com/jishi/jishi/ui/fragment/MessageFragment.java
@@ -1,5 +1,6 @@
 package com.jishi.jishi.ui.fragment;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +12,8 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -30,6 +33,8 @@ import java.util.List;
  * @date 2020/2/18 12:07
  */
 public class MessageFragment extends Fragment {
+    private LinearLayout btn_message_chat, btn_message_friendslist;
+    private TextView txv_chat, txv_friendList;
     private ListView listView;
     private List<MessageListItemViewModel> messageListItemViewModels = new ArrayList<>();
     private MessageListAdapter adapter;
@@ -55,7 +60,83 @@ public class MessageFragment extends Fragment {
 
         adapter = new MessageListAdapter(getContext(), messageListItemViewModels);
         initView();
+
+        initListener();
+
         perpareView();
+    }
+
+    private void initView() {
+        listView = getView().findViewById(R.id.lv_message_list);
+        btn_message_chat = getView().findViewById(R.id.btn_message_chat);
+        btn_message_friendslist = getView().findViewById(R.id.btn_message_friendslist);
+        txv_chat = getView().findViewById(R.id.txv_message_chat);
+        txv_friendList = getView().findViewById(R.id.txv_message_friendlist);
+
+        listView.setAdapter(adapter);
+    }
+
+    private void initListener() {
+        final LinearLayout.LayoutParams bigLayoutParams = new LinearLayout
+                .LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 7);
+        final LinearLayout.LayoutParams smailLayoutParams = new LinearLayout
+                .LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 3);
+
+        final ValueAnimator animatorChat = ValueAnimator.ofFloat(3, 7);
+        animatorChat.setInterpolator(new LinearInterpolator());
+        animatorChat.setDuration(500);
+        animatorChat.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                bigLayoutParams.weight = (float) animation.getAnimatedValue();
+                btn_message_chat.setLayoutParams(bigLayoutParams);
+                smailLayoutParams.weight = 10 - (float) animation.getAnimatedValue();
+                btn_message_friendslist.setLayoutParams(smailLayoutParams);
+                if (animation.getAnimatedFraction() > 0.9) {
+                    txv_chat.setVisibility(View.VISIBLE);
+                    txv_friendList.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        final ValueAnimator animatorFriend = ValueAnimator.ofFloat(3, 7);
+        animatorFriend.setInterpolator(new LinearInterpolator());
+        animatorFriend.setDuration(500);
+        animatorFriend.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                bigLayoutParams.weight = (float) animation.getAnimatedValue();
+                btn_message_friendslist.setLayoutParams(bigLayoutParams);
+                smailLayoutParams.weight = 10 - (float) animation.getAnimatedValue();
+                btn_message_chat.setLayoutParams(smailLayoutParams);
+                if (animation.getAnimatedFraction() > 0.9) {
+                    txv_chat.setVisibility(View.GONE);
+                    txv_friendList.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        btn_message_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bigLayoutParams.setMargins(0, 0, 2, 0);
+                smailLayoutParams.setMargins(2, 0, 0, 0);
+                v.setSelected(true);
+                btn_message_friendslist.setSelected(false);
+                animatorChat.start();
+            }
+        });
+
+        btn_message_friendslist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bigLayoutParams.setMargins(2, 0, 0, 0);
+                smailLayoutParams.setMargins(0, 0, 2, 0);
+                v.setSelected(true);
+                btn_message_chat.setSelected(false);
+                animatorFriend.start();
+            }
+        });
     }
 
     private void perpareView() {
@@ -75,17 +156,7 @@ public class MessageFragment extends Fragment {
             numNewMsg.setVisibility(View.VISIBLE);
         }
 
-    }
-
-    private void initView() {
-        listView = getView().findViewById(R.id.lv_message_list);
-
-        listView.setAdapter(adapter);
-    }
-
-
-    public static void toolbarUsage(Toolbar toolbar) {
-        toolbar.setTitle("消息");
+        btn_message_chat.setSelected(true);
     }
 
     @Override
@@ -100,5 +171,9 @@ public class MessageFragment extends Fragment {
         assert getActivity() != null;
         menu.clear();
         getActivity().getMenuInflater().inflate(R.menu.message_menu, menu);
+    }
+
+    public static void toolbarUsage(Toolbar toolbar) {
+        toolbar.setTitle("消息");
     }
 }
